5e22dbb14c22fbd32cffd0d85aea4d6aa7460dc0
AngelTravieso <angeltraviesoc@gmail.com>
2022-11-01 22:21:50 -0400

    hola mundo

---
+++ 01-hola-mundo/main.cpp
@@ -0,0 +1,12 @@
+// Este es mi primer hola mundo
+
+#include <iostream>
+
+// cout: computer out
+// cin: computer in
+
+int main() {
+    std::cout << "Hola Mundo";
+    std::cin.get();
+    return 0;
+}
