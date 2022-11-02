9ce32e074360752d1016d2e8a02987bada1b27ff
AngelTravieso <angeltraviesoc@gmail.com>
2022-11-01 22:49:02 -0400

    tipos de datos

---
+++ 02-tipos-datos/tiposDatos.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+
+    //* entero
+    int entero = 10;
+
+    //* float
+    float flotante = 9.99;
+
+    //* double (almance mas decimales que el float)
+    double doble = 9.9898;
+
+    //* char
+    char caracter = 'a';
+
+    std::cout << caracter;
+    std::cin.get();
+
+    return 0;
+}
