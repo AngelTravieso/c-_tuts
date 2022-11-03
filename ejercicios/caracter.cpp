6bcd6ad7c4656450a82ab5c0a94c3576478729d5
Angel Travieso <angeltraviesoc@gmail.com>
2022-11-03 13:46:52 -0400

    ejercicio - vocal o consonante

---
+++ ejercicios/caracter.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main() {
+
+    char letra;
+
+    regresar:
+        std::cout << "Humano por favor ingresa una letra vocal: ";
+        std::cin >> letra;
+
+        switch(letra) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                std::cout << "Bien hecho humano, haz presionado una vocal \n";
+                break;
+            default:
+                std::cout << "No es una vocal \n";
+                goto regresar;
+                break;
+        }
+
+    system("pause");
+
+    return 0;
+}
