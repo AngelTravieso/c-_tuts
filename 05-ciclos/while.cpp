a29e7ea1f7b9146e2f2d105607bb22c8c8f50c49
Angel Travieso <angeltraviesoc@gmail.com>
2022-11-04 11:37:56 -0400

    while

---
+++ 05-ciclos/while.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // inicializar
+    int i;
+    i = 1;
+
+    // condicion
+    while(i <= 10) {
+        cout << i << "\n";
+
+        // incrementador
+        i++;
+    }
+
+    system("pause");
+
+    return 0;
+}
